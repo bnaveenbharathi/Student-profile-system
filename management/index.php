1e0d1c098f94882380583f08f312dda8f5ff64f4
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2024-11-26 00:27:21 +0530

    principle dashboard

---
+++ management/index.php
@@ -0,0 +1,37 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/dash/style.css">
+    <link rel="stylesheet" href="../static/dash/custom.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <title>Dashboard Panel</title> 
+</head>
+<body>
+     
+<?php include("./sidebar.php") ?>
+
+        <div class="dash-content">
+            <div class="overview">
+                <div class="title">
+                <i class="uil uil-user"></i>
+
+                    <span class="text">Profile</span> 
+                </div>
+               
+                <!-- Profile  -->
+                <div class="container">
+
+
+
+            </div>
+    </section>
+
+    <script src="../static/dash/script.js"></script>
+</body>
+</html>
---
+++ management/sidebar.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+
+function logout() {
+    // Unset all session variables
+    session_unset();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to the login page after logging out
+    header("Location: ../staff_login.php");
+    exit();
+}
+
+if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
+    logout(); // Call the logout function if the logout parameter is set
+}
+
+?>
+
+<nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img src="images/logo.png" alt="">
+            </div>
+            <!-- <span class="logo_name">B.Naveen Bharathi</span> -->
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li><a href="./index.php">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Home</span>
+                </a></li>
+                <li><a href="./achievements.php">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Achievements</span>
+                </a></li>
+                <li><a href="./projects.php">
+                    <i class="uil uil-chart"></i>
+                    <span class="link-name">Projects</span>
+                </a></li>
+                <li><a href="./certification.php">
+                    <i class="uil uil-thumbs-up"></i>
+                    <span class="link-name">Certifications</span>
+                </a></li>
+               
+            </ul>
+            
+            <ul class="logout-mode">
+                <li><a href="sidebar.php?logout=true">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Logout</span>
+                </a></li>
+
+                <li class="mode">
+                    <a href="#">
+                        <i class="uil uil-moon"></i>
+                    <span class="link-name">Dark Mode</span>
+                </a>
+
+                <div class="mode-toggle">
+                  <span class="switch"></span>
+                </div>
+            </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        <div class="top">
+            <i class="uil uil-bars sidebar-toggle"></i>
+            <img src="images/profile.jpg" alt="">
+            <a href="../profilesystem.php" target="_blank"><button style="background:transparent;border:none;font-size:38px;" class="viewbtn"><i class="uil uil-eye" ></i></button>
+            </a>
+        </div>
+<style>
+    body.dark .viewbtn{
+    color: white;
+}
+.viewbtn{
+    color: black;
+}
+</style>
+        
